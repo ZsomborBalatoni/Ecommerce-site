0ddf6f641e7d466a052c3c25397ed48291affb5d
ZsomborBalatoni <zsombor.balatoni@gmail.com>
2023-03-20 21:09:15 +0100

    Uploading Spring project

---
+++ backend/src/main/java/hu/g2z/ecommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package hu.g2z.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hu/g2z/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.g2z.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
